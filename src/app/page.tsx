5d331b6dc9ed7a1d11374663088e5bd7592df44e
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-02-26 15:36:21 +0100

    Fix names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
             <Testimonial
                 title={"I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth."}
                 text={"“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the  best - and most grueling - time of my life. Since completing the course, I’ve  successfully switched careers, working as a Software Engineer at a VR startup. ”"}
-                personTitle={"Daniel Clifford"}
-                name={"Verified Graduate"}
+                name={"Daniel Clifford"}
+                personTitle={"Verified Graduate"}
                 photoUrl={"/images/image-daniel.jpg"}
                 variant={"purple"}
                 quotes
@@ -22,8 +22,8 @@ export default function Home() {
             <Testimonial
                 title={"The team was very supportive and kept me motivated"}
                 text={"“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself. “"}
-                personTitle={"Jonathan Walters"}
-                name={"Verified Graduate"}
+                name={"Jonathan Walters"}
+                personTitle={"Verified Graduate"}
                 photoUrl={"/images/image-jonathan.jpg"}
                 variant={"grey"}
             />
@@ -31,8 +31,8 @@ export default function Home() {
             <Testimonial
                 title={"An overall wonderful and rewarding experience"}
                 text={"“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”"}
-                personTitle={"Jeanette Harmon"}
-                name={"Verified Graduate"}
+                name={"Jeanette Harmon"}
+                personTitle={"Verified Graduate"}
                 photoUrl={"/images/image-jeanette.jpg"}
                 variant={"white"}
             />
@@ -40,8 +40,8 @@ export default function Home() {
             <Testimonial
                 title={"Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy."}
                 text={"“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”"}
-                personTitle={"Patrick Abrams"}
-                name={"Verified Graduate"}
+                name={"Patrick Abrams"}
+                personTitle={"Verified Graduate"}
                 photoUrl={"/images/image-patrick.jpg"}
                 variant={"black"}
             />
@@ -49,8 +49,8 @@ export default function Home() {
             <Testimonial
                 title={"Such a life-changing experience. Highly recommended!"}
                 text={"“ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I’ve often referred to it during interviews as an example of my developent experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend! ”"}
-                personTitle={"Kira Whittle"}
-                name={"Verified Graduate"}
+                name={"Kira Whittle"}
+                personTitle={"Verified Graduate"}
                 photoUrl={"/images/image-kira.jpg"}
                 variant={"white"}
             />
